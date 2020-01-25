a286b0df46870d915e3407514a1a99dab391a66a
Raghu <raghu.c@appviewx.com>
2020-01-25 13:50:44 +0530

    Checking matplot

--- hello.py
+++ hello.py
@@ -1,2 +1,6 @@
-name = input("Please enter your name:")
-print("Hello "+name+"!!!")
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
+plt.plot(x, np.sin(x))       # Plot the sine of each x point
+plt.show()                   # Display the plot
